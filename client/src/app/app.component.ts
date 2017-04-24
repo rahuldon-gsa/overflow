2db228180fadafcbc225c2aa31afbfd69cdea02a
Rahul Dongre <rahul.dongre@itgfirm.com>
2017-04-24 10:26:48 -0400

    Timeout Updated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component , ViewChild, ElementRef} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
 import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
 import { Keepalive } from '@ng-idle/keepalive';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalEventsManager } from './shared/services/global-events-manager';
+import { AuthenticationService } from "./shared/services/authentication.service";
 
 @Component({
   selector: 'app',
@@ -11,15 +12,16 @@ import { GlobalEventsManager } from './shared/services/global-events-manager';
 })
 export class AppComponent {
 
-closeResult: string;
+  closeResult: string;
 
   idleState = 'Not started.';
   timedOut = false;
   lastPing?: Date = null;
 
- @ViewChild('modalTimeout') timeoutModal:ElementRef;
+  @ViewChild('modalTimeoutButton') timeoutModal: ElementRef;
 
-  constructor( private modalService: NgbModal, private globalEventsManager: GlobalEventsManager, private toastyService: ToastyService, private toastyConfig: ToastyConfig, private idle: Idle, private keepalive: Keepalive) {
+  constructor(private authenticationService: AuthenticationService, private globalEventsManager: GlobalEventsManager,
+    private toastyService: ToastyService, private toastyConfig: ToastyConfig, private idle: Idle, private keepalive: Keepalive) {
 
     this.toastyConfig.theme = 'material';
 
@@ -53,9 +55,9 @@ closeResult: string;
 
   kickMeOut() {
     // sets an idle timeout of 5 seconds, for testing purposes.
-    this.idle.setIdle(5);
+    this.idle.setIdle(3);
     // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
-    this.idle.setTimeout(5);
+    this.idle.setTimeout(3);
     // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
 
@@ -63,14 +65,20 @@ closeResult: string;
     this.idle.onTimeout.subscribe(() => {
       this.idleState = 'Timed out!';
       this.timedOut = true;
-      // pop up modal component
-      this.timeoutModal.nativeElement.click();
+
+      if (this.timedOut && sessionStorage.getItem('userRole') != null) {
+        // pop up modal component
+        this.timeoutModal.nativeElement.click();
+      } else {
+        this.reset();
+      }
+
     });
     this.idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
     this.idle.onTimeoutWarning.subscribe((countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
 
     // sets the ping interval to 15 seconds
-    this.keepalive.interval(15);
+    this.keepalive.interval(5);
 
     this.keepalive.onPing.subscribe(() => this.lastPing = new Date());
 
@@ -83,28 +91,12 @@ closeResult: string;
     this.timedOut = false;
   }
 
-  clickMethod(name: string) {
-  if(alert("Are you sure to delete "+name)) {
-    console.log("Implement delete functionality here");
-  }
-}
 
-
-  open(content) {
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
+  logoutCurrentUser() {
+    this.authenticationService.logout().subscribe(res => {
+      location.reload();
+      this.globalEventsManager.showMessage("User logged out due to session timeout !!");
     }
+    );
   }
 }
--- client/src/app/shared/validators/alert-message.js
+++
@@ -1,1 +0,0 @@
-window.setTimeout(function () { $(".global-message").fadeTo(500, 0).slideUp(500, function () { $(this).remove(); }); }, 200); 
