e4253980e11519e6743074d40ae6500e8f7c68c4
Rahul Dongre <rahul.dongre@itgfirm.com>
2017-04-24 21:23:39 -0400

    Timeout updated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -17,8 +17,11 @@ export class AppComponent {
   idleState = 'Not started.';
   timedOut = false;
   lastPing?: Date = null;
+  isTimeRunningOut: any;
 
   @ViewChild('modalTimeoutButton') timeoutModal: ElementRef;
+  @ViewChild('modalTimeoutHideButton') timeoutModalHide: ElementRef;
+
 
   constructor(private authenticationService: AuthenticationService, private globalEventsManager: GlobalEventsManager,
     private toastyService: ToastyService, private toastyConfig: ToastyConfig, private idle: Idle, private keepalive: Keepalive) {
@@ -55,18 +58,40 @@ export class AppComponent {
 
   kickMeOut() {
     // sets an idle timeout of 5 seconds, for testing purposes.
-    this.idle.setIdle(3);
+    this.idle.setIdle(600);
     // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
-    this.idle.setTimeout(3);
+    this.idle.setTimeout(60);
     // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
 
-    this.idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
+    this.idle.onIdleEnd.subscribe(() => {
+      //this.idleState = 'No longer idle.';
+
+      // This will disappear timeout modal box
+      /*
+        this.reset();      
+        this.timeoutModalHide.nativeElement.click();
+      */
+
+    });
     this.idle.onTimeout.subscribe(() => {
-      this.idleState = 'Timed out!';
+      this.idleState = 'Timed out! Logging out for safety !!';
       this.timedOut = true;
 
-      if (this.timedOut && sessionStorage.getItem('userRole') != null) {
+      if (this.timedOut) {
+        this.logoutCurrentUser();
+      }
+
+    });
+    this.idle.onIdleStart.subscribe(() => {
+      // this.idleState = 'You\'ve gone idle!'
+    }
+    );
+    this.idle.onTimeoutWarning.subscribe((countdown) => {
+      this.isTimeRunningOut = true;
+      this.idleState = 'You will time out in ' + countdown + ' seconds!';
+
+      if (this.isTimeRunningOut && sessionStorage.getItem('userRole') != null) {
         // pop up modal component
         this.timeoutModal.nativeElement.click();
       } else {
@@ -74,11 +99,9 @@ export class AppComponent {
       }
 
     });
-    this.idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
-    this.idle.onTimeoutWarning.subscribe((countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
 
     // sets the ping interval to 15 seconds
-    this.keepalive.interval(5);
+    this.keepalive.interval(500);
 
     this.keepalive.onPing.subscribe(() => this.lastPing = new Date());
 
@@ -89,6 +112,7 @@ export class AppComponent {
     this.idle.watch();
     this.idleState = 'Started.';
     this.timedOut = false;
+    this.isTimeRunningOut = false;
   }
 
 
